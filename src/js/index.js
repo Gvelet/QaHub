a464a2a8103c0cba0c21acc0998cef6556b75077
Milem <milemx11@gmail.com>
2024-12-13 01:23:31 +0500

    QA-2 added header and footer

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,30 @@
-import mobileNav from './modules/mobile-nav.js';
-mobileNav();
+import Swiper from 'swiper/bundle';
+
+const swiper = new Swiper('.slider__content', {
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+
+    slidesPerView: 3,
+    spaceBetween: 30,
+    watchOverflow: true,
+});
+
+const spoilers = document.querySelectorAll('.resources__spoilers-item');
+const spoilersTexts = document.querySelectorAll('.resources__spoilers-text');
+
+spoilers.forEach((spoiler, index) => {
+    const spoilersText = spoilersTexts[index];
+
+    spoiler.addEventListener('click', () => {
+            spoilersText.classList.toggle('show');
+            spoiler.classList.toggle('show');
+    });
+});
+
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,11 @@
 const config = {
 	mode: 'production',
 	entry: {
-		index: './src/js/index.js'
+		index: './src/js/index.js',
 		// contacts: './src/js/contacts.js',
 		// about: './src/js/about.js',
+		// carousel: './node_modules/slick-carousel/slick/slick.js',
+		// jQuery: './node_modules/jquery/dist/jquery.min.js'
 	},
 	output: {
 		filename: '[name].bundle.js',
