ac1790567601b4870699a78ade31010220be5359
Milem <milemx11@gmail.com>
2024-12-14 21:16:57 +0500

    fix home page bugs

--- src/js/index.js
+++ src/js/index.js
@@ -18,13 +18,16 @@ const swiper = new Swiper('.slider__content', {
 
 const spoilers = document.querySelectorAll('.resources__spoilers-item');
 const spoilersTexts = document.querySelectorAll('.resources__spoilers-text');
+const pluses = document.querySelectorAll('.resources-plus')
 
 spoilers.forEach((spoiler, index) => {
     const spoilersText = spoilersTexts[index];
+    const plus = pluses[index];
 
     spoiler.addEventListener('click', () => {
             spoilersText.classList.toggle('show');
             spoiler.classList.toggle('show');
+            plus.classList.toggle('transform-Rotate45')
     });
 });
 
